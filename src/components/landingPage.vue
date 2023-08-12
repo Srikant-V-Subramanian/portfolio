ffdf435c16fe6e060dcd9f90dc7ac93847794dae
Srikant Subramanian <srikantvenkatsubramanian@gmail.com>
2023-08-12 18:56:41 +0530

    Update landingPage.vue

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -1,17 +1,14 @@
 <template>
   <h1 class="msg hidden">Hello!</h1>
   <p class="hidden">I'm Srikant! A developer cum music producer</p>
-  <div class="bottom-curve">
+  <div class="custom-shape-divider-bottom-1691758750">
     <svg
       data-name="Layer 1"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1200 120"
       preserveAspectRatio="none"
     >
-      <path
-        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-        class="shape-fill"
-      ></path>
+      <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
     </svg>
   </div>
 </template>
@@ -44,7 +41,7 @@ body {
   gap: 3em;
 }
 
-.bottom-curve {
+.custom-shape-divider-bottom-1691758750 {
   position: absolute;
   bottom: 0;
   left: 0;
@@ -54,14 +51,14 @@ body {
   transform: rotate(180deg);
 }
 
-.bottom-curve svg {
+.custom-shape-divider-bottom-1691758750 svg {
   position: relative;
   display: block;
-  width: calc(156% + 1.3px);
-  height: 204px;
+  width: calc(100% + 1.3px);
+  height: 186px;
 }
 
-.bottom-curve .shape-fill {
+.custom-shape-divider-bottom-1691758750 .shape-fill {
   fill: #ffffff;
 }
 </style>
